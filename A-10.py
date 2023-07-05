e7768f14b171b83f5b0fd9b88a842f25808505f5
yokochoyokocho <gotcha.pipi.new@gmail.com>
2023-07-05 11:42:27 +0900

    easy_10

---
+++ A-10.py
@@ -0,0 +1,7 @@
+import random
+dice = (1, 2, 3, 4, 5, 6)
+
+idx = random.randint(0, 5)
+result = dice[idx]
+
+print(result)
--- A-4.py
+++ A-4.py
@@ -1,4 +1,2 @@
 members = ["Bob", "Tom", "Ken"]
 print(members[0], members[1])
-
-
